e626871129a303e0993f54f600e6f86c7bdb4254
Tomy8s <tomy8s@hotmail.com>
2016-10-05 15:27:24 +0100

    step 15: added basic tests for JourneyLog

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,1 +1,18 @@
 require 'journey_log'
+
+describe JourneyLog do
+
+  describe '#initialize' do
+    it 'accepts one parameter' do
+      expect(subject).to respond_to(:initialize).with(1).argument
+    end
+  end
+  
+  describe '#start' do
+    it { is_expected.to respond_to(:start).with(1).argument }
+  end
+
+  describe '#finish' do
+    it { is_expected.to respond_to(:finish).with(1).argument }
+  end
+end

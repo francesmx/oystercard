de6fb343afa62cdc02d0dc6cf66f3dcfb501b3aa
Frances Maxwell <fmaxwell@gmail.com>
2016-10-05 15:45:00 +0100

    Failing test for starting new journey

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,3 +1,12 @@
 class JourneyLog
 
+  def initialize(journey_class)
+  end
+
+  def start(station)
+  end
+
+  def finish(station)
+  end
+
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,14 +2,17 @@ require 'journey_log'
 
 describe JourneyLog do
 
-  describe '#initialize' do
-    it 'accepts one parameter' do
-      expect(subject).to respond_to(:initialize).with(1).argument
-    end
+  subject do
+    JourneyLog.new("journey_class")
   end
-  
+
   describe '#start' do
     it { is_expected.to respond_to(:start).with(1).argument }
+
+    it 'should start a new journey with an entry station' do
+      subject.start("A")
+      expect(subject.history.count).to eq 1
+    end
   end
 
   describe '#finish' do

614b6e764c95d63f73e74cd80789ddb9f1a8db64
Tomy8s <tomy8s@hotmail.com>
2016-10-05 09:59:25 +0100

    step 14: added test to check for complete journey

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -11,7 +11,30 @@ describe Journey do
     it 'has a default exit_station of nil' do
       expect(subject.exit_station).to eq nil
     end
-
   end
 
+  let(:card) { double(:oystercard, balance: 90) }
+
+  describe '#complete?' do
+    it 'returns true when complete journey' do
+      subject.entry_station = 'Station X'
+      subject.exit_station = 'Station Y'
+      expect(subject).to be_complete 
+    end
+    it 'returns false when not touched out' do
+      subject.entry_station = 'Station X'
+      expect(subject).not_to be_complete
+    end
+    it 'returns false when not touched in' do
+      subject.exit_station = 'Station Y'
+      expect(subject).not_to be_complete
+    end
+  end
+
+  describe '#fare' do
+    # it 'charges minimum fare for a valid journey' do
+      
+    #   expect()
+    # end
+  end
 end

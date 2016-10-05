c984ff695817546b239799e2e3c52569ea6d430a
Tomy8s <tomy8s@hotmail.com>
2016-10-05 17:54:38 +0100

    step 15: Trying to get it to log both valid and invalid journeys

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -6,30 +6,33 @@ class JourneyLog
 
   def initialize(journey_class)
     @history = []
+    @journey = Journey.new
   end
 
-  def start(station)
-    current_journey(station)
+  def start(start_station)
+    current_journey(start_station)
     @history << @journey
   end
 
-  def finish(station)
+  def finish(finish_station)
     @f = true
-    current_journey(station)
-    @history.last.finish = station
+    current_journey(finish_station)
+    @history.last.finish = finish_station
     @f = false
   end
 
   private
 
   def current_journey(station)
-    if !@history.empty? && (@history.last.finish.nil? || @history.last.start.nil?)
-      @journey
-    elsif @f && (@history.empty? || @journey.complete?)
-      @journey = Journey.new(nil, station)
-      @history << @journey
-    elsif @history.empty? || @journey.complete?
+    if @history.empty? || @journey.complete? #Start an new journey under normal circumstances
       @journey = Journey.new(station)
+    elsif @f && (@history.empty? || @journey.complete?) #Finish an invalid journey (no tounch in)
+      @history << Journey.new(nil, station)
+    elsif !@history.last.start.nil? && @history.last.finish.nil? #Finish an invalid journey (no touch out)
+      @history << @journey
+      @journey = Journey.new(station)
+    elsif @history.last.finish.nil? #Finish a valid journey
+      @journey
     end
   end
 
